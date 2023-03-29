f9ed3e63e3068be04144339afe280d606a14a17c
Tran Minh Man <96435164+TranMinhMan040702@users.noreply.github.com>
2023-03-29 17:10:48 +0700

    UI Address

--- reactjs-frontend/shopmt/src/layouts/Web/components/Account/Profile.js
+++ reactjs-frontend/shopmt/src/layouts/Web/components/Account/Profile.js
@@ -1,6 +1,70 @@
 import images from '../../../../assets/images';
+import { useEffect, useState } from 'react';
+import { regex } from '../../../../utils';
+import UserService from '../../../../services/UserService';
 
-function Profile() {
+function Profile({ account }) {
+    const [user, setUser] = useState({
+        id: '',
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        address: '',
+        avatar: '',
+    });
+    const [avatar, setAvatar] = useState(null);
+    const [preview, setPreview] = useState(null);
+    useEffect(() => {
+        if (account) {
+            setUser({
+                id: account.id,
+                firstname: account.firstname,
+                lastname: account.lastname,
+                email: account.email,
+                phone: account.phone,
+                address: account.address,
+                avatar: account.avatar,
+            });
+        }
+    }, [account]);
+    useEffect(() => {
+        if (user.avatar) {
+            setAvatar(() => {
+                const bits = user.avatar.split('.');
+                const file = new File([bits[0]], user.avatar, {
+                    type: 'text/plain',
+                });
+                return { file };
+            });
+        }
+    }, [user]);
+    const handleChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+    const handleUploadAvatar = (e) => {
+        setAvatar({ file: e.target.files[0] });
+        setPreview(URL.createObjectURL(e.target.files[0]));
+    };
+    useEffect(() => {
+        return () => {
+            avatar && URL.revokeObjectURL(preview);
+        };
+    }, [avatar]);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = new FormData();
+        data.append('file', avatar.file);
+        data.append('model', JSON.stringify(user));
+        if (regex.regexPhoneNumber(user.phone)) {
+            try {
+                const response = await UserService.updateUser(data);
+                console.log(response.data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    };
     return (
         <div className="wapper">
             <div className="header">
@@ -8,31 +72,27 @@ function Profile() {
                 <p>Quản lý hồ sơ để bảo mật tài khoản</p>
             </div>
             <div className="account">
-                <form action="" className="">
+                <form action="" className="" onSubmit={handleSubmit}>
                     <div className="d-flex justify-content-between">
                         <div className="profile-info">
                             <div className="mb-3">
-                                <label htmlFor="">Tên đăng nhập</label>
-                                <span>man0407</span>
-                            </div>
-                            <div className="mb-3">
                                 <label htmlFor="">Tên</label>
-                                <input className="input-control" type="text" value="Trần Minh Mẫn" />
+                                <span>{user.firstname + ' ' + user.lastname}</span>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="">Email</label>
-                                <input className="input-control" type="text" value="mantm040702@gmail.com" />
+                                <span>{user.email}</span>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="">Số điện thoại</label>
-                                <input className="input-control" type="text" value="0964294799" />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="">Địa chỉ</label>
-                                <textarea
+                                <input
                                     className="input-control"
                                     type="text"
-                                    value="ktx khu b đại học quốc gia Phường Linh Trung, Thành Phố Thủ Đức, TP. Hồ Chí Minh"
+                                    value={user.phone}
+                                    name="phone"
+                                    required
+                                    onChange={(e) => handleChange(e)}
+                                    placeholder="Vui lòng nhập số điện thoại . . ."
                                 />
                             </div>
                             <div className="mb-3">
@@ -45,6 +105,7 @@ function Profile() {
                                         type="radio"
                                         value="male"
                                     />
+
                                     <label htmlFor="male">Nam</label>
                                 </span>
                                 <span>
@@ -57,6 +118,16 @@ function Profile() {
                                     />
                                     <label htmlFor="female">Nữ</label>
                                 </span>
+                                <span>
+                                    <input
+                                        id="other"
+                                        name="gender"
+                                        className="input-control"
+                                        type="radio"
+                                        value="other"
+                                    />
+                                    <label htmlFor="female">Khác</label>
+                                </span>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="">Ngày sinh</label>
@@ -65,10 +136,10 @@ function Profile() {
                         </div>
                         <div className="profile-img d-flex flex-column justify-content-center align-items-center">
                             <div className="img">
-                                <img src={images.noAvatar} alt="" />
+                                <img src={!avatar ? images.noAvatar : preview} alt="" />
                             </div>
                             <label htmlFor="image">
-                                <input type="file" id="image" hidden />
+                                <input accept="image/*" type="file" id="image" hidden onChange={handleUploadAvatar} />
                                 Chọn ảnh
                             </label>
                         </div>
--- reactjs-frontend/shopmt/src/layouts/Web/components/Account/index.js
+++ reactjs-frontend/shopmt/src/layouts/Web/components/Account/index.js
@@ -1,22 +1,26 @@
 import './account.scss';
+import { accountUser } from '../../../../redux/selectors';
+import { useSelector } from 'react-redux';
 import Navbar from './Navbar';
 import Profile from './Profile';
 import Password from './Password';
 import Purchase from './Purchase';
+import Address from './Address';
 import { useParams } from 'react-router-dom';
 function Account() {
+    const account = useSelector(accountUser);
     let param = useParams()['slug'];
     param = param[0].toUpperCase() + param.slice(1);
-    const components = [Profile, Password, Purchase];
+    const components = [Profile, Address, Password, Purchase];
     let Component = components.find((component) => component.name === param);
     return (
         <div className="account background">
             <div className="container d-flex justify-content-between">
                 <div className="nav">
-                    <Navbar />
+                    <Navbar account={account} />
                 </div>
                 <div className="main">
-                    <Component />
+                    <Component account={account} />
                 </div>
             </div>
         </div>
